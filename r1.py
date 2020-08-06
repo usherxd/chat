bb50dc29a4f13ba0251889186a8ff3402b67ffb4
usherxd <usherxd@gmail.com>
2020-08-06 20:48:59 +0800

    basic

---
+++ r1.py
@@ -0,0 +1,40 @@
+
+# 讀取檔案
+def read_file(filename):
+    lines = []
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+# 格式轉換
+def convert(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+
+        new.append(person + '： ' + line)
+    return new
+
+
+def write_file(filename, lines):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        for line in lines:
+            f.write(line + '\n')
+
+
+def main():
+    filename = 'input.txt'
+    lines = read_file(filename)
+    lines = convert(lines)
+    write_file('output.txt', lines)
+
+
+main()
